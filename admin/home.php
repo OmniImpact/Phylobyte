e379fc2c0b77b38f4e2a7bf79b2c9c78ebd702f3
omniuni <daniel@omniimpact.com>
2012-06-06 18:57:34 -0400

    Removed some old branding

--- admin/home.php
+++ admin/home.php
@@ -6,7 +6,7 @@ $this->pageArea = '
 <img src="gfx/logo_color_md.png" />
 <div style="float: right; width: 80%;">
 	<h2>Love your website. Make it grow.</h2>
-	<h3>Phylobyte Version 0.3 (Alpha) for VetMedCases</h3>
+	<h3>Phylobyte Version 0.3 (Alpha)</h3>
 </div>
 
 ';
